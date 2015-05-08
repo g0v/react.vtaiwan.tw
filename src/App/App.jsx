6a03812eb523d7ebd465f231fb5c6708949eda8a
Yuan Hsiang Cheng <lyforever62@hotmail.com>
2015-05-08 16:46:40 +0800

    Toggle NavBar

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -10,35 +10,36 @@ import {RouteHandler} from 'react-router'
 import ProposalList from '../ProposalList/ProposalList.jsx'
 import Category from '../Category/Category.jsx'
 
+import {NavList} from './data/Nav'
 
 moment.locale(window.navigator.userLanguage || window.navigator.language)
 
 class App extends React.Component {
     static propTypes = { id: React.PropTypes.string }
     constructor(props) { super(props)
-        this.state = { rev: null }
+        this.state = {
+            rev: null,
+            showNavBar: false
+        }
     }
     componentWillMount() { this.props.setQueryParams(this.props) }
     componentWillReceiveProps(nextProps) {
         if (nextProps.id == this.props.id) return
         this.props.setQueryParams({ id: nextProps.id })
     }
+    handleNavBar () {
+        console.log(this);
+        this.setState({ showNavBar: !this.state.showNavBar });
+    }
     render() {
         const {id, revs, onChange} = this.props;
         var rev = revs.length ? this.state.rev : null;
-        var data = [
-            { path: '/', label: '首頁' },
-            { label: '主題'},
-            { path: '/how', label: '如何發言' },
-            { path: '/tutorial', label: '使用手冊' },
-            { path: '/about', label: '關於' }
-        ];
 
         return (
             <div className="App">
-                <NavBar nav_list={data} />
-                <AppBar />
-                <div className="App-content">
+                { this.state.showNavBar? <NavBar nav_list={NavList} /> : null }
+                <AppBar handleNavBar={this.handleNavBar.bind(this)} />
+                <div className={ this.state.showNavBar? "App-content activeNavBar" : "App-content"} >
                     <div className="App-wrapper">
                         <RouteHandler />
                     </div>
--- src/AppBar/AppBar.jsx
+++ src/AppBar/AppBar.jsx
@@ -7,12 +7,18 @@ import './AppBar.css'
 moment.locale(window.navigator.userLanguage || window.navigator.language)
 
 class AppBar extends React.Component {
-    
+
     render() {
-        
+
         var imgURL = require("./images/AppBar-logo.png");
         return (
             <div className="AppBar">
+                <div className="AppBar-navCtrl" onClick={this.props.handleNavBar}>
+                    <span className="fa-stack">
+                        <i className="fa fa-circle fa-stack-2x" />
+                        <i className="fa fa-bars fa-stack-1x fa-inverse" />
+                    </span>
+                </div>
                 <div className="AppBar-siteLogo">
                     <img className="AppBar-siteLogoImg"
                          src={imgURL}/>
