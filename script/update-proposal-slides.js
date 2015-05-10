451537908860e18954e2806a46614d95a8aed8e8
Pomin Wu <pomin5@gmail.com>
2015-05-10 09:36:17 +0800

    add slides_embed_url to update script

--- script/update-proposal-slides.js
+++ script/update-proposal-slides.js
@@ -9,8 +9,9 @@ co(function* () {
   var data = JSON.parse(fs.readFileSync(proposalDataFile))
   yield Object.keys(data).map(function (name) {
     return superagent.get('http://www.slideshare.net/api/oembed/2?format=json&url=' + data[name].slides_url)
-      .then(function (res) {
-        data[name].slides_image = res.body.slide_image_baseurl.replace(/slide-$/, '-1-638.jpg')
+      .then(function ({body}) {
+        data[name].slides_image = body.slide_image_baseurl.replace(/slide-$/, '-1-638.jpg')
+        data[name].slides_embed_url = body.html.split(/"(https:\/\/www.slideshare.net\/slideshow\/embed_code\/.*?)"/)[1]
       })
     })
   fs.writeFileSync(proposalDataFile, JSON.stringify(data, null, 2))
