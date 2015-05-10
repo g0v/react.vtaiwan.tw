8106f642b895421bdc1cd3e354bf335b1e2ae527
Pomin Wu <pomin5@gmail.com>
2015-05-10 09:26:42 +0800

    use superagent-bluebird-promise in update script

--- script/update-proposal-slides.js
+++ script/update-proposal-slides.js
@@ -1,18 +1,17 @@
 
-import superagent from 'superagent'
+import superagent from 'superagent-bluebird-promise'
 import fs from 'mz/fs'
 import co from 'co'
 
 var proposalDataFile = './src/Proposal/data/Proposals.json'
-var data = JSON.parse(fs.readFileSync(proposalDataFile))
 
 co(function* () {
-  yield Object.keys(data).map((name) => new Promise(function (resolve, reject) {
-    superagent.get('http://www.slideshare.net/api/oembed/2?format=json&url=' + data[name].slides_url)
-      .end(function (err, res) {
+  var data = JSON.parse(fs.readFileSync(proposalDataFile))
+  yield Object.keys(data).map(function (name) {
+    return superagent.get('http://www.slideshare.net/api/oembed/2?format=json&url=' + data[name].slides_url)
+      .then(function (res) {
         data[name].slides_image = res.body.slide_image_baseurl.replace(/slide-$/, '-1-638.jpg')
-        resolve(true)
       })
-  }))
+    })
   fs.writeFileSync(proposalDataFile, JSON.stringify(data, null, 2))
 }).catch((err) => console.log(err))
