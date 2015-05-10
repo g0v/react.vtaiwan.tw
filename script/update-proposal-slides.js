c00ee1697caa6378621782b4216c0e180dac7b85
Pomin Wu <pomin5@gmail.com>
2015-05-10 09:47:40 +0800

    drop protocols from slides url

--- script/update-proposal-slides.js
+++ script/update-proposal-slides.js
@@ -11,7 +11,7 @@ co(function* () {
     return superagent.get('http://www.slideshare.net/api/oembed/2?format=json&url=' + data[name].slides_url)
       .then(function ({body}) {
         data[name].slides_image = body.slide_image_baseurl.replace(/slide-$/, '-1' + body.slide_image_baseurl_suffix)
-        data[name].slides_embed_url = body.html.split(/"(https:\/\/www.slideshare.net\/slideshow\/embed_code\/.*?)"/)[1]
+        data[name].slides_embed_url = body.html.split(/"(?:https?:)(\/\/www.slideshare.net\/slideshow\/embed_code\/.*?)"/)[1]
       })
     })
   fs.writeFileSync(proposalDataFile, JSON.stringify(data, null, 2))
