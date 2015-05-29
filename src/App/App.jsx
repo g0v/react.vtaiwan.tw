abf2f3859c9c051486306588ced505875f463e76
Audrey Tang <audreyt@audreyt.org>
2015-05-29 17:14:18 +0800

    * Default NavList to null so there is actually a menu to show

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -21,7 +21,7 @@ class App extends React.Component {
     constructor(props) { super(props)
         this.state = {
             showNavBar: false,
-            navList: []
+            navList: null
         }
     }
     componentWillMount() {
