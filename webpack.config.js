f4598aa24cd9b4d8a15e922ee085a43e042b385e
Audrey Tang <audreyt@audreyt.org>
2015-06-06 00:01:11 +0800

    * Not using paths.js anymore

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,6 @@ var StaticSiteGeneratorPlugin = require('static-site-generator-webpack-plugin');
 var plugins = [];
 var entries = [ './src/index' ];
 var loaders = [ 'babel?stage=0' ];
-var paths = require('./paths');
 
 if (/production/.test(process.env.NODE_ENV)) {
     plugins = [
