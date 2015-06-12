8271f22deb479a5f5435229c1cd07c93fc06cb5d
soidid <soidid@gmail.com>
2015-06-12 17:07:39 +0800

    add non-gitbook labeled links.

---
+++ src/LabeledLinks/LabeledLinks.jsx
@@ -0,0 +1,50 @@
+"use strict"
+import React from 'react'
+import moment from 'moment'
+import Transmit from 'react-transmit'
+import './LabeledLinks.css'
+
+class LabeledLinks extends React.Component {
+   
+    render() {
+        const { data } = this.props
+        var linkItems = data.links.map((v,i)=>{
+        	var icon = "";
+        	switch(v.title){
+        		case '共筆':
+        			icon = "fa fa-pencil"
+        			break
+        		case '重播':
+        			icon = "fa fa-youtube-play"
+        			break
+        		case 'PDF':
+        			icon = "fa fa-file-pdf-o"
+        			break
+
+        	}
+        	
+        	return (
+        		<a className="LabeledLinks-item"
+        		   href={v.link}
+        		   key={i}
+        		   target="_blank">
+        		   
+        		   <i className={icon}></i> 
+        		   <span className="LabeledLinks-label">{v.title}</span>
+        		</a>
+        	)
+        })
+        
+        return (
+
+            <div className="LabeledLinks">
+               
+               <div className="LabeledLinks-title">{data.title}</div>
+               {linkItems}
+            </div>
+        )
+    }
+}
+export default Transmit.createContainer(LabeledLinks, {
+    
+})
--- src/Proposal/Proposal.jsx
+++ src/Proposal/Proposal.jsx
@@ -6,6 +6,7 @@ import './Proposal.css'
 
 import Proposals from './data/Proposals.json'
 import Stage from '../Stage/Stage.jsx'
+import LabeledLinks from '../LabeledLinks/LabeledLinks.jsx'
 
 class Proposal extends React.Component {
     componentWillMount() {
@@ -29,19 +30,52 @@ class Proposal extends React.Component {
         this.props.setQueryParams(nextProps)
     }
     render() {
-        const {data} = this.props
-        var stages = data.stages.map((item, key)=>{
-            var start_date = moment(new Date(item.start_date)).format('YYYY-MM-DD');
-            return (
-                <div className="Proposal-stage"
-                     key={key}>
-                    <div className="Proposal-stageDate">{start_date}</div>
-                    <Stage data={item}
-                           gitbookURL={item.gitbook_url}
-                           categoryNum={item.category_num}
-                           proposalName={data.title_eng} />
-                </div>
-            )
+        const { data } = this.props
+
+        var timelineItems = []
+        data.stages.map((item, key)=>{
+            timelineItems.push(item)
+        });
+
+        (data.links || []).map((item, key)=>{
+            timelineItems.push(item)
+        });
+        console.log(timelineItems);
+        timelineItems.sort((a, b)=>{
+            var dateA = moment(new Date(a.start_date || a.date))
+            var dateB = moment(new Date(b.start_date || b.date))
+            
+            return dateB-dateA
+        });
+
+
+        var stages = timelineItems.map((item, key)=>{
+            var content="";
+            if(item.start_date){
+                var start_date = moment(new Date(item.start_date)).format('YYYY-MM-DD');
+                content = (
+                    <div className="Proposal-stage">
+                        <div className="Proposal-stageDate">{start_date}</div>
+                        <Stage data={item}
+                               gitbookURL={item.gitbook_url}
+                               categoryNum={item.category_num}
+                               proposalName={data.title_eng} />
+                    </div>
+                )
+
+            }else{//
+                var date = moment(new Date(item.date)).format('YYYY-MM-DD');
+                content = (
+                    <div className="Proposal-stage">
+                        <div className="Proposal-stageDate">{date}</div>
+                        <LabeledLinks data={item} />
+                    </div>
+                )
+
+            }
+
+           
+            return <div key={key}>{content}</div>
         })
 
         return (
