5210344bd2bf721c394c8657773e27335debfeff
Audrey Tang <audreyt@audreyt.org>
2016-03-08 21:57:08 +0800

    * Sketch of NTA eliquor

--- src/App/data/Nav.js
+++ src/App/data/Nav.js
@@ -1,6 +1,7 @@
 module.exports = {
   NavList: [
     { label: '主題'},
+    { path: '/eliquor/', label: '網路賣酒配套措施', icon: 'MOFTA.png', type: 'sub'},
     { path: '/airbnb/', label: 'Airbnb 自用住宅招租', icon: 'motc_tb.png', type: 'sub'},
     { path: '/uberx/', label: 'UberX 自用車載客', icon: 'motc.png', type: 'sub'},
     { path: '/cyberbullying/', label: '網路霸凌', icon: 'MOJ-MOHW.png', type: 'sub'},
