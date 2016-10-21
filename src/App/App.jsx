6cd70e88083ccbd4f8f6721398c05f034d35e892
Chung-Min Cheng <min427@gmail.com>
2016-10-21 17:50:07 +0900

    Setting up the resize monitor.

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,62 +1,84 @@
-"use strict"
-import React from 'react'
-import Transmit from 'react-transmit'
-import './App.css'
+'use strict';
+import React from 'react';
+import Transmit from 'react-transmit';
+import './App.css';
 
-import AppBar from '../AppBar/AppBar.jsx'
-import NavBar from '../NavBar/NavBar.jsx'
-import {RouteHandler} from 'react-router'
+import AppBar from '../AppBar/AppBar.jsx';
+import NavBar from '../NavBar/NavBar.jsx';
+import {RouteHandler} from 'react-router';
 
-import Nav, {NavList} from './data/Nav'
-import categoryData from '../Category/data/Category'
+import Nav, {NavList} from './data/Nav';
+import categoryData from '../Category/data/Category';
 
 class App extends React.Component {
-    static propTypes = { id: React.PropTypes.string }
-    static contextTypes = { router: React.PropTypes.func }
-    constructor(props) { super(props)
-        var showNavBar = true; //window.innerWidth < 600 ? false:true;
+  static propTypes = { id: React.PropTypes.string }
+  static contextTypes = { router: React.PropTypes.func }
+  static viewportWidth = 600;
 
-        this.state = {
-            showNavBar: showNavBar,
-            navList: null
-        }
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showNavBar: true,
+      navList: null,
+      onClickSticky: false
+    };
+  }
+
+  handleResize() {
+    if (typeof window === 'undefined') {
+      return;
     }
-    componentWillMount() {
-        if(typeof window !== 'undefined' && window.screen.availWidth <= 600) {
-            this.setState({ showNavBar: false });
-        }
+
+    this.state.showNavBar = window.innerWidth <= App.viewportWidth;
+    this.handleNavBar();
+  }
+
+  componentDidMount() {
+    this.handleResize();
+
+    window.addEventListener('resize', this.handleResize.bind(this), false);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize.bind(this), false);
+  }
+
+  handleNavBar () {
+    this.setState({ showNavBar: !this.state.showNavBar });
+  }
+
+  setNavList(navList) {
+    var prevNavList = this.state.navList
+
+    if (JSON.stringify(navList) === JSON.stringify(prevNavList)) {
+      return;
     }
-    componentWillReceiveProps(nextProps) {
-    }
-    handleNavBar () {
-        this.setState({ showNavBar: !this.state.showNavBar });
-    }
-    setNavList(navList) {
-        const prevNavList = this.state.navList
-        if (JSON.stringify(navList) === JSON.stringify(prevNavList)) { return }
-        this.setState({navList, prevNavList})
-    }
-    render() {
-        var {router} = this.context;
-        var {proposalName, category} = router.getCurrentParams();
-        // var coverImg = require("./images/cover_small.jpg");
-        return (
-            <div className="App">
-                <NavBar nav_list={ this.state.navList }
-                        handleNavBar={this.handleNavBar.bind(this)}
-                        activePath={router.getCurrentPath()}
-                        showNavBar={this.state.showNavBar}/>
-                <AppBar handleNavBar={this.handleNavBar.bind(this)} />
-                <div className={ this.state.showNavBar ? "App-content activeNavBar" : "App-content"} >
-                    <div className="App-wrapper">
-                    <RouteHandler setNavList={this.setNavList.bind(this)} />
-                    </div>
-                </div>
-            </div>
-        )
-    }
+
+    this.setState({navList, prevNavList})
+  }
+
+  render() {
+    var {router} = this.context;
+    var {proposalName, category} = router.getCurrentParams();
+    // var coverImg = require("./images/cover_small.jpg");
+    return (
+      <div className="App">
+        <NavBar nav_list={ this.state.navList }
+                handleNavBar={this.handleNavBar.bind(this)}
+                activePath={router.getCurrentPath()}
+                showNavBar={this.state.showNavBar}/>
+        <AppBar handleNavBar={this.handleNavBar.bind(this)} />
+        <div class="App-content" className={ this.state.showNavBar ? 'activeNavBar' : ''} >
+          <div className="App-wrapper">
+            <RouteHandler setNavList={this.setNavList.bind(this)} />
+          </div>
+        </div>
+      </div>
+    )
+  }
 }
 
 export default Transmit.createContainer(App, {
-    queries: {}
+  queries: {}
 })
